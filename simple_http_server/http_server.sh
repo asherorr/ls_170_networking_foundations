69dfdbb6d2397effdd77f0f19e559ed824e2d50d
asherorr <103453685+asherorr@users.noreply.github.com>
2025-05-16 17:17:29 +0000

    This commit will update the http server to provide debugging info in the case of an error.

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-function server () {
-  while true
-  do
-    message_arr=()
-    check=true
-    while $check
-    do
-      read line
-      message_arr+=($line)
-      if [[ "${#line}" -eq 1 ]]
-      then
-        check=false
-      fi
+function server() {
+  while true; do
+    # Read the request line: METHOD PATH VERSION
+    if ! IFS=' ' read -r method path version; then
+      # End of input or error - exit loop
+      break
+    fi
+
+    # Debug print (uncomment to see requests)
+    # echo "DEBUG: Received request: $method $path $version" >&2
+
+    # Read and discard all HTTP headers until an empty line
+    while IFS= read -r header_line; do
+      [[ "$header_line" == $'\r' || -z "$header_line" ]] && break
     done
-    method=${message_arr[0]}
-    path=${message_arr[1]}
-    if [[ $method = 'GET' ]]
-    then
-      if [[ -f "./www/$path" ]]
-      then
-        echo -ne 'HTTP/1.1 200 OK\r\n\r\n'; cat "./www/$path"
+
+    # Remove leading slash from path, e.g. /lion.html -> lion.html
+    path="${path#/}"
+
+    if [[ "$method" == "GET" ]]; then
+      if [[ -f "./www/$path" ]]; then
+        content_length=$(wc -c < "./www/$path")
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $content_length\r\n\r\n"
+        cat "./www/$path"
       else
-        echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
+        echo -ne "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
       fi
     else
-      echo -ne 'HTTP/1.1 400 Bad Request\r\n\r\n'
+      echo -ne "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n"
     fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+netcat -lkv 2345 <&"${SERVER_PROCESS[0]}" >&"${SERVER_PROCESS[1]}"
